fa21b645077ac123c4bb5d044b1af1d34528f1cf
nabajyotisarkar2004-hub <nabajyotisarkar2004@gmail.com>
2025-12-17 23:02:07 +0530

    Added watch and download links

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 const movies = [
-  { id: 1, title: "Night of the Living Dead", year: 1968 },
-  { id: 2, title: "Plan 9 from Outer Space", year: 1959 },
-  { id: 3, title: "The Little Shop of Horrors", year: 1960 }
+  {
+    title: "Night of the Living Dead",
+    year: 1968,
+    link: "https://archive.org/details/night_of_the_living_dead"
+  },
+  {
+    title: "Plan 9 from Outer Space",
+    year: 1959,
+    link: "https://archive.org/details/Plan_9_from_Outer_Space_1959"
+  },
+  {
+    title: "The Little Shop of Horrors",
+    year: 1960,
+    link: "https://archive.org/details/TheLittleShopOfHorrors1960"
+  }
 ];
 
-const container = document.getElementById("movies");
+const movieList = document.getElementById("movie-list");
 
 movies.forEach(movie => {
   const div = document.createElement("div");
-  div.className = "movie";
+  div.className = "movie-card";
+
   div.innerHTML = `
     <h3>${movie.title}</h3>
     <p>Year: ${movie.year}</p>
-    <button onclick="alert('Movie ID: ${movie.id}')">View Details</button>
+    <a href="${movie.link}" target="_blank">
+      <button>Watch / Download</button>
+    </a>
   `;
-  container.appendChild(div);
+
+  movieList.appendChild(div);
 });
